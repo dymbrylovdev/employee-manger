260d44217d1410c4ee05f54400abe08f7c4f7c00
gunga2001 <tsydupovsodnom@gmail.com>
2022-02-07 13:15:34 +0800

    first commit

---
+++ src/main/java/com/dymbrylov/employeemanager/service/EmployeeService.java
@@ -0,0 +1,25 @@
+package com.dymbrylov.employeemanager.service;
+
+import com.dymbrylov.employeemanager.model.Employee;
+import com.dymbrylov.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+
+
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+}
